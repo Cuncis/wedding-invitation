05fdf2c623c663066390a36484e03b5bd131150b
Cuncis <38809651+Cuncis@users.noreply.github.com>
2026-05-14 12:06:42 +0700

    Update migration

--- database/migrations/2026_05_13_071029_add_columns_to_users_table.php
+++ database/migrations/2026_05_13_071029_add_columns_to_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -16,8 +15,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone', 20)->nullable()->after('email');
-            $table->boolean('is_admin')->default(false)->after('phone');
+            $table->string('phone')->nullable()->after('email');
+            $table->string('whatsapp')->nullable()->after('phone');
+            $table->enum('role', ['admin', 'customer'])
+                ->default('customer')->after('whatsapp');
+            $table->timestamp('last_login_at')->nullable();
         });
     }
 
